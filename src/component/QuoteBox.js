cc244b29a1333c78a686aaab3980814395d71cc0
ingjuangracia <ing.juangracia@hotmail.com>
2022-03-04 14:19:34 -0500

    Se modifica proyecto

--- src/component/QuoteBox.js
+++ src/component/QuoteBox.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const QuoteBox = ({quote, author, changeQuote, color, background }) => {
+const QuoteBox = ({quote, author, changeQuote, color }) => {
     return (
         <div className="quoteBox">
             <h4 style={{color}}><i className="fas fa-quote-left"></i>{quote}</h4>
             <h5 style={{color}}>{author}</h5>
-            <button style= {{color}} onClick={changeQuote}><i className="fas fa-angle-right"></i></button>
+            <button style= {{background:color}} onClick={changeQuote}><i className="fas fa-angle-right"></i></button>
             
         </div>
     );
